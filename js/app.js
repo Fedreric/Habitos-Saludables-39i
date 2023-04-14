a547a69d9cd13931c842929c4ea58724b5191f84
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-04-14 00:29:21 -0300

    se agrega logica para el buscador del main

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const array = ["disciplina", "ejercitacion", "habitos", "journaling", "mindfulness"];
+const list = document.getElementById("list");
+
+function filterArray() {
+    const searchTerm = document.getElementById("buscador").value.toLowerCase();
+    const filteredArray = array.filter(item =>
+        item.toLowerCase().includes(searchTerm)
+    );
+    console.log(filteredArray);
+    renderList(filteredArray);
+}
+
+function renderList(arrayFiltrado) {
+    console.log('array desde renderlist: '+ arrayFiltrado);
+    list.innerHTML = '';
+    
+    if(arrayFiltrado.length > 0) {
+        arrayFiltrado.forEach(item => {
+            list.innerHTML += `
+        <aside class="col-md-6 col-lg-4 card text-bg-light my-3 p-0">
+            <a href="pages/${item}.html">
+            <img src="img/${item}.png" alt="banner1" class="w-auto">
+            <div class="card-img-overlay tituloCard ">
+              <h5 class="card-title">${item.toUpperCase()}</h5>
+            </div>
+        </aside>`;
+        });
+    }else{
+        list.innerHTML = `<p class="text-center fs-2 fw-light fst-italic">No se encontraron articulos asociados con: ${document.getElementById("buscador").value}</p>`;
+    }
+
+}
