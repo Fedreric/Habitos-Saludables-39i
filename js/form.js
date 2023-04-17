782f2f231925997c7625700bd3734d7657b78604
Agustin Sandoval <94272906+Agustincomics@users.noreply.github.com>
2023-04-16 23:13:13 -0300

    Creacion logica del comentario

---
+++ js/form.js
@@ -0,0 +1,36 @@
+document.getElementById("formulario").addEventListener("submit", comentar);
+
+function comentar(event){
+    event.preventDefault();
+    let fechaActual = new Date();
+    let dia = fechaActual.getDate();
+    let año = fechaActual.getFullYear();
+    let mes = fechaActual.getMonth()+1;
+    let hora = fechaActual.getHours();
+    let minutos = fechaActual.getMinutes();
+    if (minutos < 10) {
+        minutos = '0' + minutos;
+      } 
+    let nombre = document.getElementById("nombre_input").value;
+    let comentario = document.getElementById("txt-tarea").value;
+    let comentarioSection = document.getElementById("comentarios");
+    /* let nuevoComentario = document.createElement("article"); */
+    comentarioSection.innerHTML = "<strong>" + nombre + `</strong> ${dia}/${mes}/${año} a las ${hora}:${minutos}<br>` + comentario;
+ /*    comentarioSection.appendChild(nuevoComentario); */
+    document.getElementById("formulario").reset();
+}
+
+
+
+/* let boton = document.getElementById("botonEnviar");
+boton.addEventListener("click", comentar);
+let sector = document.getElementById("sector");
+
+function comentar(){
+    let nombre = document.getElementById("nombre_input").value;
+    let nNombre = document.createElement("p");
+    nNombre.innerHTML = `${nombre}`;
+    console.log(nNombre);
+    sector.insertBefore(nNombre, boton);
+} */
+
